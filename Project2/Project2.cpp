f9d7c730ff9c7fade74d20ca862089d8577b3630
MrGeleos <mr.geleos12@mail.ru>
2022-05-05 21:31:21 +0300

    Added a folder with the program

---
+++ Project2/Project2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A, B, S;
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите число A и B\n";
+    cout << "A: ";
+    cin >> A;
+    cout << "B: ";
+    cin >> B;
+    S = A + B;
+    cout << "Сумма чисел А и B равна " << S << endl;
+    return 0;
+}
